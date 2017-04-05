6f48ac7851689071c50e9824bc72fde5fa6b888f
Dennis Hübner <dennis.huebner89@gmail.com>
2017-04-05 09:51:40 +0200

    Mongo example is now working, AFTER DAYS..cargo cache sucks :D

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,23 @@ extern crate mongodb;
 
 use chrono::prelude::*;
 use std::collections::HashSet;
+use std::env;
 use mongodb::{Client, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
-use mongodb::coll::options::{FindOptions, FindOneAndUpdateOptions, IndexModel, IndexOptions,
-                             ReturnDocument};
-
-
-#[derive(Debug, Hash, Eq, PartialEq)]
-struct Money {
-    item: String,
-    euros: i32,
-    cents: i32,
-    date: DateTime<Local>,
-}
+use mongodb::coll::options::{FindOneAndUpdateOptions, IndexModel, IndexOptions, ReturnDocument};
+use bson::Bson;
 
 fn main() {
 
-    let client = Client::connect("localhost", 27042).ok().expect("Failed");
-    let db = client.db("test");
+    let mut client: Client = connect_to_db();
 
-    let coll = db.list_collections(None).ok().expect("Authorized?");
+    #[derive(Debug, Hash, Eq, PartialEq)]
+    struct Money {
+        item: String,
+        euros: i32,
+        cents: i32,
+        date: DateTime<Local>,
+    }
 
     type Income = Money;
     type Outcome = Money;
@@ -43,7 +40,6 @@ fn main() {
         println!("{:?}", &test_inc);
         incomes.insert(test_inc);
     }
-
     for x in 0..3 {
         let test_ouc = Outcome {
             item: String::from("MORE MONEY"),
@@ -54,4 +50,29 @@ fn main() {
         println!("{:?}", &test_ouc);
         outcomes.insert(test_ouc);
     }
+}
+
+fn connect_to_db() -> Client {
+    let key = "MONGOPORT";
+    let client = match env::var(key) {
+        Ok(val) => Client::connect("localhost", val.parse::<u16>().unwrap()).ok().expect("Failed"),
+        _ => Client::connect("localhost", 27017).ok().expect("Failed"),
+    };
+
+    let coll = client.db("test").collection("test");
+
+    let mut cursor = coll.find(None, None).unwrap();
+
+    let item = cursor.next();
+    match item {
+        Some(Ok(doc)) => {
+            match doc.get("x") {
+                Some(&Bson::I32(ref title)) => println!("{}", title),
+                _ => panic!("Expected title to be a string!"),
+            }
+        }
+        Some(Err(_)) => panic!("Failed to get next from server!"),
+        None => panic!("Server returned no results!"),
+    }
+    client
 }
